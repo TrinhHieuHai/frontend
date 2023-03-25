27f3eb9899b7d3df107880609899f0f595517b5a
TrinhHieuHai <118946394+TrinhHieuHai@users.noreply.github.com>
2023-03-25 11:57:30 +0700

    frontend

---
+++ src/components/admin/Dashboard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Dashboard() {
+    return  (
+        <center>
+            <h1>Website bán hàng React-Laravel</h1>
+            <h1>Các thành viên trong nhóm</h1>
+            <ul>
+                <h1>
+                1.	Trịnh Hiếu Hải	MSSV: DH51901579	Lớp:	D19_TH05 <br></br>
+                2.	Nguyễn Thành Đạt	MSSV: DH51903413	Lớp:	D19_TH05 <br></br>
+                3.	Thiều Văn Vũ Tín	MSSV: D51801021	Lớp:	<br></br>
+                4.	Hồ Đoan Trang	MSSV: LT52100007	Lớp:	<br></br>
+                </h1>
+            </ul>
+        </center>
+    )
+}
+
+export default Dashboard;
